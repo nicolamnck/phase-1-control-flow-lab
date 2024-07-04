1bc4fe776833b36f559f1162091229e2089254e8
¨NicolaMarie¨ <nicolamarie.nm@gmail.com>
2024-07-05 00:55:35 +0300

    completed labs

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
+  if(distance <= 400)
+  {return 'This one is on me!'}
+
+  if(distance >= 400&& distance <= 2000)
+    {return 'That will be twenty bucks.'}
+
+  if(distance > 2000 && distance <= 2500)
+    {return 'I will gladly take your thirty bucks.'}
+
+  if(distance > 2500)
+    {return 'No can do.'}
+
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  if(city == 'NYC')
+    {return "Ok, sounds good." }
+   else return "No go."
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  if(tip == 'generous')
+    {return "Thank you so much." }
+
+   if(tip == 'not as generous')
+    {return "Thank you."}
+   else return 'Bye.'
+
+   
   // Write your code here!
 }
